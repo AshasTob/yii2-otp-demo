150cdb7872f66ef719877f1b5ce58e3ad9b6e1f3
LingKing <bratokokok@gmail.com>
2017-04-17 14:55:44 +0300

    Configuration commit
    Further project cleanup;

--- config/web.php
+++ config/web.php
@@ -3,13 +3,13 @@
 $params = require(__DIR__ . '/params.php');
 
 $config = [
-    'id' => 'basic',
+    'id' => 'otp-demo',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => '1QOPGJBKFNSVMBJ932GN5FPVBS',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -37,15 +37,13 @@ $config = [
                 ],
             ],
         ],
-        'db' => require(__DIR__ . '/db.php'),
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
-        ],
-        */
+        ]
     ],
     'params' => $params,
 ];
